3b4221b424025aa5d5b0236825e7a90d078b69cf
Bikcode <bikcodeh@gmail.com>
2022-09-24 17:39:51 -0500

    add new endpoint to search a recipe by id

--- app/src/main/java/com/bikcodeh/modernfoodapp/data/remote/api/FoodRecipeApi.kt
+++ app/src/main/java/com/bikcodeh/modernfoodapp/data/remote/api/FoodRecipeApi.kt
@@ -1,8 +1,11 @@
 package com.bikcodeh.modernfoodapp.data.remote.api
 
+import com.bikcodeh.modernfoodapp.BuildConfig
 import com.bikcodeh.modernfoodapp.data.remote.response.FoodResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 interface FoodRecipeApi {
@@ -15,5 +18,13 @@ interface FoodRecipeApi {
     @GET("recipes/complexSearch")
     suspend fun searchRecipes(
         @QueryMap query: Map<String, String>
+    ): Response<FoodResponse>
+
+    @GET("recipes/{recipeId}/information")
+    suspend fun searchRecipeById(
+        @Query("addRecipeInformation") recipeInformation: String = "true",
+        @Query("fillIngredients") fillIngredients: String = "true",
+        @Query("apiKey") key: String = BuildConfig.FOOD_KEY,
+        @Path("recipeId") recipeId: Int
     ): Response<FoodResponse>
 }
